80be836ad586188da0a0fdef06d8c28d176e3da1
stylesuxx <stylesuxx@gmail.com>
2013-08-13 00:27:21 +0200

    Parser example. Count occurance of foo. Command fc to print occurances of foo

---
+++ plugins/foo/foo.py
@@ -0,0 +1,33 @@
+import abc
+from plugin import Parser
+
+class ParserImplementation(Parser):
+    _name = 'foo counter'
+    _command = 'fc'
+    _help = 'Show the foo count.'
+    _description = 'Show the foo count'
+    _public = True
+
+    def __init__(self):
+        self.count = 0
+
+    def name(self):
+        return self._name
+
+    def command(self):
+        return self._command
+
+    def help(self):
+        return self._help
+
+    def description(self):
+        return self._description
+
+    def public(self):
+        return self._public
+
+    def process(self, args):
+        return 'Foo count: ' + str(self.count)
+
+    def parse(self, msg):
+        self.count += msg.count("foo")
