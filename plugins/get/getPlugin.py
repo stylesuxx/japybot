6411bce3ea45abae238596a823ed5b0e4dd1e79a
stylesuxx <stylesuxx@gmail.com>
2013-08-13 22:39:33 +0200

    Added get Plugin. This Plugin enables admins to download plugins from remote destination. Do not forget to reload after downloading the plugins.

---
+++ plugins/get/getPlugin.py
@@ -0,0 +1,43 @@
+import abc, urllib2, re
+from plugin import Command
+
+class CommandImplementation(Command):
+    _name = 'get'
+    _command = 'get'
+    _help = 'Get one or more plugins from a remote destination. [get URL1 URL2]'
+    _description = 'Enables the admin users to download one or more plugins from a remote destination.'
+    _public = False
+    _version = "0.1"
+
+    def name(self):
+        return self._name
+
+    def version(self):
+        return self._version
+
+    def command(self):
+        return self._command
+
+    def help(self):
+        return self._help
+
+    def description(self):
+        return self._description
+
+    def public(self):
+        return self._public
+
+    def process(self, args, isAdmin):
+        if isAdmin:
+            toReturn = "Downloads:\n"
+            urls = args.split(' ')
+            for url in urls:
+                if re.match("^http.*\.zip$", url):
+                    try:
+                        f = urllib2.urlopen(url)
+                        with open("plugins/" + url.split('/')[-1], "wb") as code:
+                            code.write(f.read())    
+                        toReturn += url + " - OK\n"
+                    except urllib2.HTTPError, e:
+                        toReturn += url + " - Failed (" + str(e.code) + ")\n"
+        return toReturn
