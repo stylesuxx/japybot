8f44fc79da4238e2554639b478f2cf600f853db1
stylesuxx <stylesuxx@gmail.com>
2013-08-15 03:45:58 +0200

    Added functionality to create user on server if the given username is not registered.

--- bot.py
+++ bot.py
@@ -3,12 +3,13 @@ from plugin import Plugin, Command, Parser
 from pluginLoader import PluginLoader
 
 class Bot:
-    def __init__(self, jid, pwd, admins):
+    def __init__(self, jid, pwd, admins, register):
         self.jid = xmpp.JID(jid)
         self.user = self.jid.getNode()
         self.domain = self.jid.getDomain()
         self.pwd = pwd
         self.admins = admins
+        self.register = register
         self.conn = xmpp.Client(self.domain)#, debug=[])
         self.ignore = []
         path = os.getcwd() + '/plugins/'
@@ -30,6 +31,10 @@ class Bot:
 
         self.authres = self.conn.auth(self.user, self.pwd, 'pybot')
         if not self.authres:
+            # If option is enabled and given user is not registered with the server try to do so.
+            if self.register and xmpp.features.register(self.conn, self.domain, {'username': self.user, 'password': self.pwd}):
+                self.register = False
+                self.connect()
             print "Unable to authorize on %s - check login/password." %self.domain
             sys.exit(1)
         
@@ -127,11 +132,10 @@ parser.add_argument('-s', dest='server', metavar='SERVER', nargs='?', help='The 
 parser.add_argument('-r', dest='room', metavar='ROOM', nargs='?', help='The room to join')
 parser.add_argument('-n', dest='nick', metavar='NICK', nargs='?', help='The nick for the room')
 parser.add_argument('-p', dest='passRoom', metavar='PASS', nargs='?', help='The password for the room')
-#TODO
 parser.add_argument('--reg', dest='register', action='store_const', const=True, default=False, help='Register Jid if available')
 
 args = vars(parser.parse_args())
-bot = Bot(args['user'], args['pass'], args['admins'])
+bot = Bot(args['user'], args['pass'], args['admins'], args['register'])
 bot.loadPlugins()
 bot.connect()
 if args['room'] and args['server'] and args['nick']:
